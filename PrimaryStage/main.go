e3acf727d93bb902accba8f6ccc2d034f4df31fb
kezi <wow.kimsohyun.wu@gamil.com>
2022-01-28 00:06:10 +0800

    2022-01-27 Daily

--- PrimaryStage/main.go
+++ PrimaryStage/main.go
@@ -1,9 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+	"strings"
+	"unicode"
+)
 
+func main() {
+	fmt.Println(strings.ContainsRune("!.,", '!'))
 }
 
+// 存在重复元素Ⅱ
 func containsNearbyDuplicate(nums []int, k int) bool {
 	n := len(nums)
 	set := map[int]bool{}
@@ -17,4 +25,42 @@ func containsNearbyDuplicate(nums []int, k int) bool {
 		set[nums[i]] = true
 	}
 	return false
+}
+
+// 石子游戏Ⅸ
+func stoneGameIX(stones []int) bool {
+	cnts := make([]int, 3)
+	for _, stone := range stones {
+		cnts[stone%3]++
+	}
+	if cnts[0]%2 == 0 {
+		return !(cnts[1] == 0 || cnts[2] == 0)
+	} else {
+		return !(math.Abs(float64(cnts[1]-cnts[2])) <= 2)
+	}
+}
+
+// 句子中的有效单词数
+func countValidWords(sentence string) (ans int) {
+	for _, s := range strings.Fields(sentence) {
+		if valid(s) {
+			ans++
+		}
+	}
+	return
+}
+func valid(s string) bool {
+	hasHyphene := false
+	for i, ch := range s {
+		if unicode.IsDigit(ch) || strings.ContainsRune("!.,", ch) && i < len(s)-1 {
+			return false
+		}
+		if ch == '-' {
+			if hasHyphene || i == 0 || i == len(s)-1 || !unicode.IsLower(rune(s[i-1])) || !unicode.IsLower(rune(s[i+1])) {
+				return false
+			}
+			hasHyphene = true
+		}
+	}
+	return true
 }
