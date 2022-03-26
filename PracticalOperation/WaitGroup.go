3173942a508e74a7f2efe21038eb0b59e97c950c
Kezi <wow.kimsohyun.wu@gmail.com>
2022-03-26 23:37:42 +0800

    daily0326

---
+++ PracticalOperation/WaitGroup.go
@@ -0,0 +1,30 @@
+package main
+
+import "sync"
+
+type httppkg struct {
+}
+
+func (httppkg) Get(url string) {
+
+}
+
+var http httppkg
+
+func main() {
+	var wg sync.WaitGroup
+	var urls = []string{
+		"http://www.golang.org/",
+		"http://www.google.com/",
+		"http://www.somestupidname.com/",
+	}
+
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			http.Get(url)
+		}(url)
+	}
+	wg.Wait()
+}
--- PrimaryStage/main.go
+++ PrimaryStage/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"strconv"
 	"strings"
 	"unicode"
 )
@@ -84,4 +85,28 @@ func reverseOnlyLetters(s string) string {
 		right--
 	}
 	return string(ans)
+}
+
+//leetcode-682 棒球比赛
+func calPoints(ops []string) (ans int) {
+	points := []int{}
+	for _, op := range ops {
+		n := len(points)
+		switch op[0] {
+		case '+':
+			ans += points[n-1] + points[n-2]
+			points = append(points, points[n-1]+points[n-2])
+		case 'D':
+			ans += points[n-2] * 2
+			points = append(points, 2*points[n-2])
+		case 'C':
+			ans -= points[n-1]
+			points = points[:len(points)-1]
+		default:
+			pt, _ := strconv.Atoi(op)
+			ans += pt
+			points = append(points, pt)
+		}
+	}
+	return
 }
