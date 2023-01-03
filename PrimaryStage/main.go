78f009369f3c545788ebdf3f0ccee6f1e474782c
kezi <wow.kimsohyun.wu@gamil.com>
2023-01-03 22:37:38 +0800

    2023-01-03 Daily

--- PrimaryStage/main.go
+++ PrimaryStage/main.go
@@ -406,3 +406,18 @@ func Max(i int, i2 int) int {
 	}
 	return i2
 }
+
+//检查句子中的数字是否递增
+func areNumbersAscending(s string) bool {
+	pre := 0
+	for _, t := range strings.Split(s, " ") {
+		if t[0] <= '9' {
+			cur, _ := strconv.Atoi(t)
+			if pre >= cur {
+				return false
+			}
+			pre = cur
+		}
+	}
+	return true
+}
