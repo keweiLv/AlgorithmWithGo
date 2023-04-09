9e591b6d710e2c35c11d4dc1adeafe9024ef4341
kezi <wow.kimsohyun.wu@gmail.com>
2023-04-09 23:19:54 +0800

    2023-04-09 daily

--- PrimaryStage/main.go
+++ PrimaryStage/main.go
@@ -466,3 +466,16 @@ func confusingNumber(n int) bool {
 	}
 	return origin != reverse
 }
+
+// 检查相同字母间的距离
+func checkDistances(s string, distance []int) bool {
+	d := [26]int{}
+	for i, c := range s {
+		c -= 'a'
+		if d[c] > 0 && i-d[c] != distance[c] {
+			return false
+		}
+		d[c] = i + 1
+	}
+	return true
+}
